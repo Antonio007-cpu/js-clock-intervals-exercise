b9d870f89ebe8aef285c9905db78a3a82b5ff4a9
antonio00727 <jmarrero3483@gmail.com>
2022-06-30 22:39:51 -0400

    finished producted

--- clock.js
+++ clock.js
@@ -13,17 +13,17 @@ const Degrees = (up, tick) =>{
 const clockMove =() => {
     segundos++
     const segundosDegrees = Degrees(segundos,60)
-    secHand.style.transform = `rotate(${segundosDegrees}deg)`
+    secHand.style.transform = `rotate(${segundosDegrees})`
     if ((segundos %60)==0){
         minutos++
         const minutosDegrees = Degrees(minutos,60)
-        minHand.style.transform = `rotate(${minutosDegrees}deg)`
+        minHand.style.transform = `rotate(${minutosDegrees})`
     }
     if ((segundos % 3600) == 0){
         horas++
         const horasDegrees = Degrees(horas,12)
-        hourHand.style.transform = `rotate(${horasDegrees}deg)`
+        hourHand.style.transform = `rotate(${horasDegrees})`
     }
 }
 
-const seconds = setInterval(clockMove, 1)
+const seconds = setInterval(clockMove, 5)
